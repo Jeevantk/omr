36aa78fa2f88244cb2343ab6b7a712be94e5f72c
jeevantk <jeevan.thomaskoshy@gmail.com>
2017-05-31 10:55:18 +0530

    added remove text powered by Tesseract

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 all:
 	mkdir -p build
 	g++ -o build/linedetect src/linedetect.cpp `pkg-config --cflags --libs opencv`
+	g++ -o build/makedataset src/makedataset.cpp `pkg-config --cflags --libs opencv`
+	g++ -o build/trim src/trim.cpp `pkg-config --cflags --libs opencv`
+	g++ -o build/detectTrebleCleff src/trebleCleff.cpp `pkg-config --cflags --libs opencv`
+	g++ -o build/trainHog src/train_HOG.cpp `pkg-config --cflags --libs opencv`
 data:
 	mkdir -p build
 	g++ -o build/makedataset src/makedataset.cpp `pkg-config --cflags --libs opencv`
@@ -16,6 +20,9 @@ detectTrebleCleff:
 detectBassCleff:
 	mkdir -p build
 	g++ -o build/detectBassCleff src/bassCleff.cpp `pkg-config --cflags --libs opencv` 
+removeText:
+	mkdir -p build
+	g++ -o build/removeText src/removeText.cpp `pkg-config --cflags --libs opencv`
 
 clean:
 	rm -rf build/
--- src/removeText.cpp
+++ src/removeText.cpp
@@ -8,31 +8,12 @@
 
 using namespace cv;
 using namespace std;
-//using namespace tesseract;
 using namespace cv::text;
 
 Mat img;
 vector <int> box_heights;
-//This function calculates the number of text lines in the code.The number of detected lines includes false detection also,majorly that including 'i' or 'l' from
-// This function is actually not needed anymore :P
-int number_of_text_lines(vector<Rect> boxes,vector<float>  confidences,vector<string> words)
-{
-  int current_y_coordinate=0;
-  int no_of_lines=0;
-  for(int i=0;i<boxes.size();i++)
-  {
-    if (words[i].find_first_not_of(' ') != std::string::npos && confidences[i]>70.0)
-    {
-      if((boxes[i].y-current_y_coordinate)>boxes[i].height/2)
-      {
-        current_y_coordinate=boxes[i].y;
-        no_of_lines++;
-      }
-    }
-  }
-  return no_of_lines;
 
-}
+
 vector<int> y_coordinates_of_various_lines_of_text(vector<Rect> boxes,vector<float> confidences,vector<string> words)
 {
   vector <int> y_coordinates;
@@ -107,12 +88,12 @@ int main(int argc, char** argv)
     }
 
     Mat gray;
+    namedWindow("Original Image",CV_WINDOW_NORMAL);
+    imshow("Original Image",img);
     cvtColor(img, gray, CV_BGR2GRAY);
     Size size(2200/img.rows*img.cols,2200);
     resize(img,img,size);
     resize(gray,gray,size);
-    namedWindow("original image",CV_WINDOW_NORMAL);
-    imshow("original image",img);
     //namedWindow("resized",WINDOW_NORMAL);
     //imshow("resized",gray);
     string output;
@@ -123,60 +104,20 @@ int main(int argc, char** argv)
 
     //NULL,NULL,"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY",3,3
     Ptr<OCRTesseract> ocr = OCRTesseract::create();
-    //ocr.char_whitelist("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    
     ocr->run(gray, output, &boxes, &words, &confidences, OCR_LEVEL_WORD);
-    // for(int i=0;i<boxes.size();i++)
-    // {
-    //   rectangle(img, boxes[i].tl(), boxes[i].br(),Scalar(255,0,0), 2, 8, 0 );
-    // }
-    // imshow("simply tesseract",img);
-    // waitKey(0);
+
     Scalar color=Scalar(255,0,0);
-    /*for (int i=0;i<boxes.size();i++)
-    {
-      if (words[i].find_first_not_of(' ') != std::string::npos && confidences[i]>70.0)
-      {
-        //imshow("individual Boxes",gray(boxes[i]));
-        rectangle(img, boxes[i].tl(), boxes[i].br(),color, 2, 8, 0 );
-        //namedWindow("image",WINDOW_NORMAL);
-        //imshow("image",img);
-        //cout<<words[i]<<endl;
-        //cout<<confidences[i]<<endl;
-        //cout<<boxes[i].y<<endl;
-        //waitKey(0);
-      }
-
-
-      //rectangle(img, boxes[i].tl(), boxes[i].br(), color, 2, 8, 0 );
-    }*/
-    //cout<<number_of_text_lines(boxes,confidences,words)<<endl;
 
     vector<int> y_coords=y_coordinates_of_various_lines_of_text(boxes,confidences,words);
     cout <<"Number of lines of text detected is "<<y_coords.size()<<endl;
     //cout <<y_coords.size()<<endl;
     for(int i=0;i<y_coords.size();i++)
     {
-      //cout<<y_coords[i]<<endl;
-      //line(img,Point(0,y_coords[i]),Point(img.cols-1,y_coords[i]),Scalar(255,0,0),4,8,0);
+
       rectangle(img,Point(0,y_coords[i]-box_heights[i]/4),Point(img.cols,y_coords[i]+3*box_heights[i]/2),Scalar(255,255,255),-1,8);
     }
-    //threshold(gray,gray, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-    /*TessBaseAPI ocr;
-    ocr.Init(NULL, "eng",OEM_DEFAULT);
-    ocr.SetPageSegMode(PSM_SINGLE_BLOCK);
-    ocr.SetImage((uchar*)gray.data, gray.cols, gray.rows, 1, gray.cols);
 
-    char* out = ocr.GetUTF8Text();
-
-    cout << out << endl;
-    */
-    /*threshold(gray,gray, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-    ocr.SetImage((uchar*)gray.data, gray.cols, gray.rows, 1, gray.cols);
-    out = ocr.GetUTF8Text();
-    cout << out << endl;*/
-    //cout << output <<endl;
-    //imwrite("output1.jpg",img);
-    //waitKey(0);
     namedWindow("lines detected",CV_WINDOW_NORMAL);
     imshow("lines detected",img);
     //imwrite("lines.jpg",img);
